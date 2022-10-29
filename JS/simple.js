133121dd96e6db6c1115479f43b8a576bfcc5ff5
Andrew Rono <ndrwrono2001@gmail.com>
2022-10-29 14:05:27 +0300

    worked with switch case

--- JS/simple.js
+++ JS/simple.js
@@ -14,3 +14,32 @@ email address is: ${email}
 var authenticated = true;
 
  authenticated ? console.log("show login button") : console.log("show signup button");
+
+
+ //switch
+
+ let user = "testprep";
+ switch(user){
+    case "admin":
+    console.log("you get the full access");
+    break;
+
+
+    case "subadmin":
+    console.log("you get access to create and delete courses");
+    break;
+
+    case "testprep":
+    console.log("you get access to create and delete tests");
+    break;
+
+    case "user":
+    console.log("you get access to consume the content")
+    break;
+
+    default:
+        console.log("you have no privillseges")
+
+
+
+ }
